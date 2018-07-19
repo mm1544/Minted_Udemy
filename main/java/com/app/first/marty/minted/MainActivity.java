69b1d93df07eb1828c6129978569ac7f19380b4f
Martynas <mm1544@york.ac.uk>
2018-07-19 15:51:12 +0100

    Initial commit

---
+++ main/java/com/app/first/marty/minted/MainActivity.java
@@ -0,0 +1,19 @@
+package com.app.first.marty.minted;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    private void TestUpload(){
+        int i = 1 + 1;
+    }
+}
+
+
